863429afb02187147510ebb4fda17d501a8a9228
Salma Ahmed <108466703+Salmaahmed8@users.noreply.github.com>
2025-01-24 21:47:09 +0200

    feat: add patch route

--- app.js
+++ app.js
@@ -64,6 +64,21 @@ app.post('/api/v1/tours', (req,res) => {
     );
 });
 
+app.patch('/api/v1/tours/:id', (req,res) => {
+
+    if(req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'failed',
+            message: 'Invalid ID'
+        });
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated tour here..>'
+        }
+    });
+});
 
 const port = 3000;
 app.listen(port, () => {
