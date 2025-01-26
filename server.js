e332855b7c1031447023414e89a4d859a7a52fdc
Salma Ahmed <108466703+Salmaahmed8@users.noreply.github.com>
2025-01-26 20:09:20 +0200

    feat: connect to DB

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
 
+dotenv.config({ path: './config.env'});
 
-const port = 3000;
+const DB = process.env.MONGO_URL;
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false
+}).then(con => {
+    console.log(con.connection);
+    console.log('DB connection successful');
+}).catch(err => {
+    console.error('Database connection error:', err)
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}..`);
 });
