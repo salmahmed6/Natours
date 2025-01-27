6abf0849447e983411fdf0d2246f88336289f3b3
Salma Ahmed <108466703+Salmaahmed8@users.noreply.github.com>
2025-01-27 20:15:10 +0200

    feat: update the tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,15 +4,37 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true,'A tour must have a name'],
-        unique: true
+        unique: true,
+        trim: true
     },
-    rating:{
+    durations: {
+        type: Number,
+        required: [true, 'A tour must have a durations']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
+    difficulty:{
+        type: String,
+        required: [true, 'A tour must have a diffuculity']
+    },
+    ratingsAverage:{
         type: Number,
         dafault: 4.5
+    },
+    ratingsQuantity:{
+        type: Number,
+        default: 0
     },
     price:{
         type: Number,
         require: [true, 'A tour must have a price']
+    },
+    priceDiscount: Number,
+    summary:{
+        type: String,
+        trim: true 
     }
 });
 
