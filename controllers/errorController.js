d4796f3fb1a8367d31d5709a599c0f5f1ebc7041
salma <sa1963645@gmail.com>
2025-02-18 11:54:19 +0200

    fix: handling mongoose validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,27 @@
 const AppError = require('../utils/appError');
 
-
 const handleCastErrorDB = (err) => {
-  const message = `Invalid ${err.message}: ${err.value}.`
-  return new AppError()
+  const message = `Invalid ${err.message}: ${err.value}.`;
+  return new AppError();
 };
 
+const handleDuplicateFieldsDB = (err) => {
+
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+
+  const message = `Duplicate field value: ${value} please use another value!`;
+  return new AppError(message, 400);
+};
+
+
+const handleValidationErrorDB = (err) => {
+  
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data.${errors.join(', ')}`;
+  return new AppError(message, 400);
+}
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -35,6 +51,9 @@ const sendErrorPod = (err, res) => {
   }
 };
 
+
+
+
 module.exports = (err, req, res, next) => {
   //console.log(err.stack);
 
@@ -44,13 +63,11 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV !== 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
 
-
-    let error = {...err};
-
-    if(err.name === 'CastError') err = handleCastErrorDB(err);
-
-
+    if (err.name === 'CastError') err = handleCastErrorDB(err);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if(error.name === 'ValidationError') error = handleValidationErrorDB(error);
     sendErrorPod(error, res);
   }
 };
