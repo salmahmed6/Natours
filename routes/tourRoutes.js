e2f10903cec60ed243de7919bda3c692a0035cd5
salma <sa1963645@gmail.com>
2025-07-01 15:17:16 +0300

    refactor: comment out unused route handlers in tourRoutes.js for clarity

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,36 +7,36 @@ const router = express.Router();
 
 // router.param('id', tourController.checkID);
 
-//create a checkbody middleware
-//check if body contains the name and price property
-//if not, send back 400(bad request)
-//add it to the post handler stack
+// //create a checkbody middleware
+// //check if body contains the name and price property
+// //if not, send back 400(bad request)
+// //add it to the post handler stack
 
-router
-  .route('/top-5-cheap')
-  .get(
-    catchAsync(tourController.aliasTopTours),
-    catchAsync(tourController.getAllTours)
-  );
+// router
+//   .route('/top-5-cheap')
+//   .get(
+//     catchAsync(tourController.aliasTopTours),
+//     catchAsync(tourController.getAllTours)
+//   );
 
-router.route('/tour-status').get(tourController.getTourStatus);
-router
-  .route('/monthly-plan/:year')
-  .get(catchAsync(tourController.getMonthlyPlan));
+// //router.route('/tour-status').get(tourController.getTourStatus);
+// router
+//   .route('/monthly-plan/:year')
+//   .get(catchAsync(tourController.getMonthlyPlan));
 
-router
-  .route('/')
-  .get(authController.protect, tourController.getAllTours)
-  .post(tourController.createTour);
+// router
+//   .route('/')
+//   .get(authController.protect, tourController.getAllTours)
+//   .post(tourController.createTour);
 
-router
-  .route('/:id')
-  .get(tourController.getTour)
-  .patch(tourController.updateTour)
-  .delete(
-    authController.protect,
-    authController.restrictTo('admin', 'lead-guide'),
-    tourController.deleteTour
-  );
+// router
+//   .route('/:id')
+//   .get(tourController.getTour)
+//   .patch(tourController.updateTour)
+//   .delete(
+//     authController.protect,
+//     authController.restrictTo('admin', 'lead-guide'),
+//     tourController.deleteTour
+//   );
 
 module.exports = router;
