35a5c09f5bff8bd72efdab71dde67b0544a62627
salma <sa1963645@gmail.com>
2025-07-07 11:06:58 +0300

    feat: add active field to user schema and filter inactive users in queries

--- models/userModel.js
+++ models/userModel.js
@@ -52,6 +52,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   changePasswordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 userSchema.pre('save', async function (next) {
@@ -70,6 +75,11 @@ userSchema.pre('save', function (next) {
   if (!this.isModified('password') || this.isNew) return next();
 
   this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.pre(/^find/, function (next) {
+  this.find({ active: { $ne: false } });
   next();
 });
 
