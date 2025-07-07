1a834d5a34fc92e364861d9ff8e94787f2fb1247
salma <sa1963645@gmail.com>
2025-07-07 11:37:57 +0300

    feat: add express-rate-limit middleware to enhance API rate limiting

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
-const jwt = require('jsonwebtoken');
+const rateLimit = require('express-rate-limit');
+
 
 const AppError = require('./utils/appErrors');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,11 +10,18 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//1- middlewares
+//1- global middlewares
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
@@ -25,7 +33,6 @@ app.use((req, res, next) => {
 });
 
 // 3- Routes
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -24,6 +24,8 @@ const createSendToken = (user, statusCode, res) => {
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
   res.cookie('jwt', token, cookieOptions);
 
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     data: {
