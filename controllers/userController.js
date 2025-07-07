8f14ee20e0d7ea44fd808074842cfb15a3bdd749
salma <sa1963645@gmail.com>
2025-07-07 12:11:15 +0300

    fix: correct import path for AppError in userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
+const AppError = require('./../utils/appErrors');
 
 const filterObj = (obj, ...allowedFields) => {
     const newObj = {};
