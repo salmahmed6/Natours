b6c625660be468088a417be15b18b52fe4ac59f1
salma <sa1963645@gmail.com>
2025-07-07 11:06:52 +0300

    feat: add updateMe and deleteMe routes in userController for user profile management

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,15 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach(el => {
+        if (allowedFields.includes(el)) newObj[el] = obj[el];
+    });
+    return newObj;
+}
+
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
     const users = await User.find();
@@ -26,24 +36,42 @@ exports.updateUser = catchAsync(async (req, res, next) => {
     });
     res.status(200).json({
     });
+});
 
-    exports.getUser = ((req, res) => {
-        res.status(500).json({
-            status: 'error ',
-            message: 'This route is not yet defined'
-        });
-    });
+exports.updateMe = catchAsync(async (req, res, next) => {
+    if (req.body.password || req.body.passwordConfirm) {
+        return next(
+            new AppError(
+                'This route is not for password updates. Please use /updateMyPassword.',
+                400
+            )
+        );
+    }
 
-    exports.updateUser = ((req, res) => {
-        res.status(500).json({
-            status: 'error ',
-            message: 'This route is not yet defined'
-        });
-    });
+    const filteredBody = filterObj(req.body, 'name', 'email');
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, { new: true, runValidators: true });
 
-    exports.deleteUser = ((req, res) => {
-        res.status(500).json({
-            status: 'error ',
-            message: 'This route is not yet defined'
-        });
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updatedUser
+        }
     });
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updatedUser
+        }
+    });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+    await User.findByIdAndUpdate(req.user.id, { active: false });
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+});
+
+
