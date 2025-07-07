b801a76a5711be7ca5ab495c1f1cc1c1c41d642d
salma <sa1963645@gmail.com>
2025-07-07 11:53:51 +0300

    feat: add helmet middleware for security and enhance JSON request limit in app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 
 const AppError = require('./utils/appErrors');
@@ -11,10 +12,15 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //1- global middlewares
+//security HTTP headers
+app.use(helmet());
+
+//development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+//limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -22,7 +28,11 @@ const limiter = rateLimit({
 });
 
 app.use('/api', limiter);
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+
+//data sanitization against NoSQL query injection
+
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
