904a2edf23b626952790d8d8978c4854765787d2
salma <sa1963645@gmail.com>
2025-07-07 11:26:47 +0300

    feat: add JWT cookie handling in createSendToken function for enhanced security

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,14 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+
+  res.cookie('jwt', token, {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  });
+
   res.status(statusCode).json({
     status: 'success',
     data: {
