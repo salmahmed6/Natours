de5a7aeb573e3a947e5030b74a43890d9948a0c9
salma <sa1963645@gmail.com>
2025-07-16 11:05:58 +0300

    refactor: streamline guide population logic in pre-find middleware for improved clarity and performance in tour schema

---
+++ models/reviewModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+    {
+        review: {
+            type: String,
+            required: [true, 'Review can not be empty']
+        },
+        rating: {
+            type: Number,
+            min: 1,
+            max: 5,
+            required: [true, 'Ratting must be between 1 and 5']
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        tour: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Tour',
+            required: [true, 'Review must belong to a tour.']
+        }
+    }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+module.exports = Review;
