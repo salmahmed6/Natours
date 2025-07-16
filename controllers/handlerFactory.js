79d38c52bb1d34f38b660af30b65267323bed279
salma <sa1963645@gmail.com>
2025-07-16 15:00:45 +0300

    refactor: update updateOne method in handlerFactory to use generic Model and improve error messaging for non-existent documents

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -16,19 +16,19 @@ exports.deleteOne = Model => catchAsync(async (req, res, next) => {
 
 
 exports.updateOne = Model = catchAsync(async (req, res, next) => {
-    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
         runValidators: true,
     });
 
-    if (!tour) {
-        return next(new AppError('No tour found with that ID', 400));
+    if (!doc) {
+        return next(new AppError('No document found with that ID', 400));
     }
 
     res.status(200).json({
         status: 'success',
         data: {
-            tour,
+            data: doc,
         },
     });
 });
