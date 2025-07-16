3fed87b5aec814740250f9620fcdecd83a6b5b16
salma <sa1963645@gmail.com>
2025-07-16 10:32:40 +0300

    refactor: comment out User import and pre-save middleware in tour schema for future adjustments

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('sulgify');
-const User = require('./userModel');
+//const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -80,7 +80,12 @@ const tourSchema = new mongoose.Schema({
             day: Number
         }
     ],
-    guides: Array
+    guides: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User'
+        }
+    ]
 },
     {
         toJSON: { virtuals: true },
@@ -98,12 +103,11 @@ tourSchema.pre('save', function (next) {
     next();
 });
 
-tourSchema.pre('save', async function (next) {
-    const guidesPromises = this.guides.map(async id => USer.findById(id));
-    this.guides = await Promise.all(guidesPromises)
-    next();
-});
-
+// tourSchema.pre('save', async function (next) {
+//     const guidesPromises = this.guides.map(async id => USer.findById(id));
+//     this.guides = await Promise.all(guidesPromises)
+//     next();
+// });
 
 
 //tourSchema.pre('save', functional(next)) {
