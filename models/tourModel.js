87757ccbddafa733af5a65014d1b8d557db9f71e
salma <sa1963645@gmail.com>
2025-07-16 10:15:43 +0300

    feat: add post-find middleware to log query execution time and pre-aggregate middleware to exclude secret tours in tour schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -113,6 +113,18 @@ tourSchema.pre(/^find/, function (next) {
     next();
 });
 
+tourSchema.post(/^find/, function (docs, next) {
+    console.log(`Query took ${Date.now() - this.start} milliseconds`);
+    next();
+})
+
+//Aggregation Midleware
+tourSchema.pre('aggregate', function (next) {
+    this.pipeline().unshift({ $match: ({ secretTour: { $ne: true } }) });
+    console.log(this.pipeline());
+    next();
+});
+
 
 const Tour = mongoose.model('Tour', tourSchema);
 
