4896da526014cf6e0f0d7d001b19c3b68c16844f
salma <sa1963645@gmail.com>
2025-07-16 16:00:51 +0300

    refactor: comment out unused AppError import and getTourStats method for cleaner code in tourController

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,6 @@
 const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appErrors.js');
+//const AppError = require('./../utils/appErrors.js');
 const factory = require('./handlerFactory.js')
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -14,6 +14,22 @@ exports.getTour = factory.getOne(Tour, { path: 'reviews' });
 exports.createTour = factory.createOne(Tour);
 exports.updateTour = factory.updateOne(Tour);
 exports.deleteTour = factory.deleteOne(Tour);
+
+// exports.getTourStats = catchAsync(async (req, res, next) => {
+//   const stats = await Tour.aggregate([
+//     {
+//       $match: { ratingsAverage: { $gte: 4.5 } },
+//     },
+//     {
+//       $group: {
+//         _id: null,
+//         numTours: { $sum: 1 },
+//         avgRating: { $avg: '$ratingsAverage' },
+//         avgPrice: { $avg: '$price' },
+//         minPrice: { $min: '$price' },
+//         maxPrice: { $max: '$price' },
+
+//       })
 
 // exports.deleteTour = catchAsync(async (req, res, next) => {
 //   const tour = await Tour.findByIdAndDelete(req, params.id);
