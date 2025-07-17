0a237bce317029f0e38439e3b8641bd8bb546efd
salma <sa1963645@gmail.com>
2025-07-17 10:18:59 +0300

    refactor: update user routes to enhance security by applying authentication middleware and changing resetPassword route to PATCH for better RESTful practices

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -10,19 +10,23 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
-router.post('/resetPassword/:token', authController.resetPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
+router.use(authController.protect);
+
+router.patch(
+  '/updateMyPassword',
+  authController.updatePassword
+);
 
 router.get(
   '/me',
-  authController.protect,
   userController.getMe,
   userController.getUser
 );
 
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
 router
   .route('/')
