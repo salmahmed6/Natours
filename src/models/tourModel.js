ead725fea6fe7fa2365764bca634eac4070eebda
salma <sa1963645@gmail.com>
2025-07-17 12:28:26 +0300

    feat: add indexes to tour schema for improved query performance

--- src/models/tourModel.js
+++ src/models/tourModel.js
@@ -99,6 +99,9 @@ const tourSchema = new mongoose.Schema({
     }
 );
 
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 tourSchema.virtual('durationWeeks').get(function () {
     return this.durations / 7;
 });
