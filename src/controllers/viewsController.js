998d1706da96099510360096d2a108196d6f6fe5
salma <sa1963645@gmail.com>
2025-07-21 14:47:03 +0300

    feat: update getTour controller to fetch tour data with reviews and render the tour template

--- src/controllers/viewsController.js
+++ src/controllers/viewsController.js
@@ -15,8 +15,18 @@ exports.getOverview = catchAsync(async (req, res, next) => {
     });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync( async(req, res, next) => {
+    //1- get the data for the requested tour including reviews and tours
+    const tour = await Tour.findOne({slug: req.params.slug}).populate({
+        path: 'reviews',
+        fields: 'review rating user'
+    })
+    //2- build template
+
+    //3- render the template using data form1 
     res.status(200).render('tour' , {
-        title: 'The Forest Hiker Tour',
+        title: `${tour.name} Tour`,
+        tour
     });
-};
+
+});
