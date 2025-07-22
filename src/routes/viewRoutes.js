d741d26bddb184a1ed02ece2b939fffd32f5db81
Salma <sa1963645@gmail.com>
2025-07-22 15:02:02 +0300

    feat: add login route to viewRoutes for rendering login form

--- src/routes/viewRoutes.js
+++ src/routes/viewRoutes.js
@@ -7,6 +7,7 @@ const router =  express.Router();
 
 router.get('/', viewsController.getOverview );
 router.get('/tour/:slug', viewsController.getTour );
+router.get('/login', viewsController.getLoginForm);
 
 
 module.exports = router;
