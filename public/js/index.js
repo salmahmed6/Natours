7a47995a277e4b6c78763627d44aeb0743a09256
salma <sa1963645@gmail.com>
2025-07-23 15:12:44 +0300

    feat: enhance user settings functionality by adding event listeners for user data and password updates

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,13 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
+import { updateSettings } from './updateSettings';
 
 //DOM elements
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form'); 
 const logOutBtn = document.querySelector('.nav_el--logout');
+const updatePasswordForm = document.querySelector('.form-user-password');
 
 // delegation
 if(mapBox){
@@ -24,4 +26,34 @@ if(loginForm){
     })
 }
 
-if(logOutBtn) logOutBtn.addEventListener('click', logout)
+if(logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if(userDataForm) {
+    userDataForm.addEventListener('submit', e => {
+        e.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        updateSettings({name, email}, 'data');
+    });
+}
+
+
+if(userPasswordForm) {
+    userPasswordForm.addEventListener('submit', async e => {
+        e.preventDefault();
+        document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+        const passwordCurrent = document.getElementById('password-current').value;
+        const password = document.getElementById('password').value;
+        const passwordConfirm = document.getElementById('password-confirm').value;
+        await updateSettings(
+            {passwordCurrent , password, passwordConfirm },
+            'password'
+        );
+
+        document.querySelector('.btn--save-password').textContent = 'Save Password';
+        document.getElementById('password-current').value = '';
+        document.getElementById('password').value = '';
+        document.getElementById('password-confirm').value = '';
+    });
+}
