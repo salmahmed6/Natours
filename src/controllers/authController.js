0b7f4b6abce2fd39d6d5370a324abfa5a3f32539
salma <sa1963645@gmail.com>
2025-07-23 10:38:27 +0300

    feat: update authController to support JWT retrieval from cookies

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -83,10 +83,15 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startWith('Bearer ')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } else if(req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
-  console.log('token: ' + token);
+
+  //console.log('token: ' + token);
   if (!token) {
-    return next(new AppError('You are not logged in! Please log in', 401));
+    return next(
+      new AppError('You are not logged in! Please log in', 401)
+    );
   }
 
   //2 verification of token
