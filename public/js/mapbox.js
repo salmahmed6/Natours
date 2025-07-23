95e9f1df200b8500885608f43f3f06a8ed157a7a
salma <sa1963645@gmail.com>
2025-07-23 11:56:13 +0300

    feat: refactor map display logic to export displayMap function and improve marker and popup handling

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,48 +1,47 @@
 /* eslint-disable */
 
-const locations = document.getElementById('map').dataset.locations;
-console.log(locations);
+export const displayMap = (locations) => {
+    mapboxgl.accessToken = 
+    'pk.eyJ1Ijoiam9uYXN2Y2htZWR0bWFubliIsImEiOiJjam54ZmM5N3gwNjAzM3dtZDNxYTVlMnd2In0.ytpIV7v7wCyT1KqsrT9Z1A';
 
-mapboxgl.accessToken = 
-'pk.eyJ1Ijoiam9uYXN2Y2htZWR0bWFubliIsImEiOiJjam54ZmM5N3gwNjAzM3dtZDNxYTVlMnd2In0.ytpIV7v7wCyT1KqsrT9Z1A';
-
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    scrollZoom: false
-    // center: [-122.084051, 37.385348],
-    // zoom: 10,
-    // interactive: false
-})
-
-const bounds = new mapboxgl.LngLatBounds();
-
-locations.forEach(loc => {
-    const el = document.createElement('div');
-    el.className = 'marker';
-
-    new mapboxgl.Marker({
-        element: el,
-        anchor: 'bottom',
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        scrollZoom: false
+        // center: [-122.084051, 37.385348],
+        // zoom: 10,
+        // interactive: false
     })
-    .setLngLat(loc.coordinates)
-    .addTo(map);
 
-    new mapboxgl.Popup({
-        offset: 30
-    })
+    const bounds = new mapboxgl.LngLatBounds();
+
+    locations.forEach(loc => {
+        const el = document.createElement('div');
+        el.className = 'marker';
+
+        new mapboxgl.Marker({
+            element: el,
+            anchor: 'bottom',
+        })
         .setLngLat(loc.coordinates)
-        .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
         .addTo(map);
 
-    bounds.extend(loc.coordinates);
-});
+        new mapboxgl.Popup({
+            offset: 30
+        })
+            .setLngLat(loc.coordinates)
+            .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+            .addTo(map);
 
-map.fitBounds(bounds, {
-    padding: {
-            top: 200,
-            bottom: 200,
-            left: 100,
-            right: 100
-        }
-}); 
+        bounds.extend(loc.coordinates);
+    });
+
+    map.fitBounds(bounds, {
+        padding: {
+                top: 200,
+                bottom: 200,
+                left: 100,
+                right: 100
+            }
+    });
+}
