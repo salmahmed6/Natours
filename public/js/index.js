70dcc3ecae17753e0e81dd697982f19e0caf9b20
salma <sa1963645@gmail.com>
2025-07-23 11:56:01 +0300

    feat: implement map display and login form functionality in index.js

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+/* eslint-discard */
+import '@babel/polyfill';
+import { displayMap } from './mapbox';
+import { login } from './login';
+
+//DOM elements
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form'); 
+
+// delegation
+if(mapBox){
+    const locations = JSON.parse(document.getElementById('map').dataset.locations);
+    displayMap(locations);
+}
+
+
+if(loginForm){
+    loginForm.querySelector('.form').addEventListener('submit', e=> {
+        e.preventDefault();
+        const email = document.querySelector('email').value;
+        const password = document.querySelector('password').value;
+        login( email, password );
+    })
+}
