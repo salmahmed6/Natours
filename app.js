7f7e613210ba6fbd4612714e103d54e91a81f6b3
salma <sa1963645@gmail.com>
2025-07-23 14:26:57 +0300

    feat: add support for URL-encoded data parsing in app.js

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use('/api', limiter);
 
 // body parser, rendering data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb'}));
 app.use(cookieParser());
 
 //data sanitization against NoSQL query injection
