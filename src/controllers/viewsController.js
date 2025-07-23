8075f928d91a3979a91ccf7b08f645daa36b2ee5
salma <sa1963645@gmail.com>
2025-07-23 13:21:09 +0300

    feat: add error handling for non-existent tours in viewsController

--- src/controllers/viewsController.js
+++ src/controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appErrors');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
     //1- get tour data from collection
@@ -21,6 +22,13 @@ exports.getTour = catchAsync( async(req, res, next) => {
         path: 'reviews',
         fields: 'review rating user'
     })
+
+    if(!tour) {
+        return next(new AppError(
+            'There is no tour with that name.',
+            404
+        ))
+    }
     //2- build template
 
     //3- render the template using data form1 
