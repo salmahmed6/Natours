15c539d27049e36a1603165c46d7444a96456897
salma <sa1963645@gmail.com>
2025-07-23 14:27:10 +0300

    feat: add account view and update user data functionality in viewsController

--- src/controllers/viewsController.js
+++ src/controllers/viewsController.js
@@ -46,3 +46,26 @@ exports.getLoginForm = catchAsync(async (req, res, next) => {
         title: 'Login into your acount',
     })
 })
+
+exports.getAccount = (req, res) => {
+    res.status(200).render('account', {
+        title: 'Your account',
+    });
+}
+
+exports.updateUserData = catchAsync(async (req, res, next) => {
+    const user = await User.findByIdAndUpdate(
+            req.user.id, {
+            name: req.body.name,
+            email: req.body.email
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+    );
+    res.status(200).render('account', {
+        title: 'Your account',
+        user: updateUser
+    });
+});
